621e11ca621ab52b74a27636edd0dac7e17d607b
Paweł Krzyżak <pawel.krzyzak@microsoft.wsei.edu.pl>
2023-05-07 19:22:21 +0200

    poprawki

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -140,6 +140,29 @@ namespace GradeBook.GradeBooks
             return gpa;
         }
 
+        public virtual char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade >= 90)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 80)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= 70)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= 60)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
 
         public virtual void CalculateStatistics()
         {
@@ -222,31 +245,6 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine(grade);
             }
         }
-
-        public virtual char GetLetterGrade(double averageGrade)
-        {
-            if (averageGrade >= 90)
-            {
-                return 'A';
-            }
-            else if (averageGrade >= 80)
-            {
-                return 'B';
-            }
-            else if (averageGrade >= 70)
-            {
-                return 'C';
-            }
-            else if (averageGrade >= 60)
-            {
-                return 'D';
-            }
-            else
-            {
-                return 'F';
-            }
-        }
-
 
         /// <summary>
         ///     Converts json to the appropriate gradebook type.
